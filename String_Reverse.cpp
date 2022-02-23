2ae48225cf232ca81763b3f06627d1af0360d329
StarkPrince <games.princeraj@gmail.com>
2022-02-24 03:16:10 +0530

    i am too lazy

---
+++ String_Reverse.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+using namespace std;
+
+///////////////////////////////////////////////////
+/*
+██████╗░██████╗░██╗███╗░░██╗░█████╗░███████╗  ██████╗░░█████╗░░░░░░██╗
+██╔══██╗██╔══██╗██║████╗░██║██╔══██╗██╔════╝  ██╔══██╗██╔══██╗░░░░░██║
+██████╔╝██████╔╝██║██╔██╗██║██║░░╚═╝█████╗░░  ██████╔╝███████║░░░░░██║
+██╔═══╝░██╔══██╗██║██║╚████║██║░░██╗██╔══╝░░  ██╔══██╗██╔══██║██╗░░██║
+██║░░░░░██║░░██║██║██║░╚███║╚█████╔╝███████╗  ██║░░██║██║░░██║╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚════╝░╚══════╝  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░
+*/
+///////////////////////////////////////////////////
+#define fast_io                            \
+    ios::sync_with_stdio(false);           \
+    cin.tie(0);                            \
+    cout.tie(0);                           \
+#define file_io                        \
+        freopen('input.txt', 'r+', stdin); \
+    freopen('output.txt', 'w+', stdout);
+
+///////////////////////////////////////////////////
+#define pv(v)             \
+    for (auto i : v)      \
+        cout << i << ' '; \
+    cout << endl;
+#define cinv(v, n)              \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+//////////////////////////////////////////////////
+typedef long long ll;
+#define int long long
+#define MOD 1000000007
+#define inf 0x3f3f3f3f
+#define minf -0x3f3f3f3f
+
+///////////////////////////////////////////////////
+#define mem(a, t) memset(a, t, sizeof(a))
+#define endl '\n'
+#define print(x) cout << x << endl
+
+///////////////////////////////////////////////////
+#define pb push_back
+#define Pb pop_back
+#define be() begin()
+#define en() end()
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define len(p) (ll) p.size()
+
+///////////////////////////////////////////////////
+
+ll binpow(ll a, ll b, ll m = 1e18)
+{
+    a %= m;
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve()
+{
+    string dup;
+    cin >> dup;
+    string og = dup;
+    ll n = dup.size();
+    reverse(dup.begin(), dup.end());
+    int i = 0, j = 0, ctr = 0;
+    while (i < n && j < n)
+    {
+        if (og[i] == dup[j])
+            j++;
+        else
+            ctr++;
+        i++;
+    }
+    print(ctr);
+}
+
+int32_t main()
+{
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
