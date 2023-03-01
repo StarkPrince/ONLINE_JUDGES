564e9a3b5f2e1f6e5d39d6f36325db21659a9cf1
StarkPrince <games.princeraj@gmail.com>
2023-03-01 06:24:06 +0530

    started all of it again :)

---
+++ codeforces/C_Dora_and_Search.cpp
@@ -0,0 +1,140 @@
+// ?Problem : https://codeforces.com/problemset/problem/1793/C
+// *Solution : maintain a sorted array which has low and high and use the main array as two pointer and see if the corner elements are present min or max
+
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////////////////////////////////////////
+/*
+██████╗░██████╗░██╗███╗░░██╗░█████╗░███████╗  ██████╗░░█████╗░░░░░░██╗
+██╔══██╗██╔══██╗██║████╗░██║██╔══██╗██╔════╝  ██╔══██╗██╔══██╗░░░░░██║
+██████╔╝██████╔╝██║██╔██╗██║██║░░╚═╝█████╗░░  ██████╔╝███████║░░░░░██║
+██╔═══╝░██╔══██╗██║██║╚████║██║░░██╗██╔══╝░░  ██╔══██╗██╔══██║██╗░░██║
+██║░░░░░██║░░██║██║██║░╚███║╚█████╔╝███████╗  ██║░░██║██║░░██║╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚════╝░╚══════╝  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░
+*/
+///////////////////////////////////////////////////
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);                 \
+    cout << fixed << setprecision(9);
+///////////////////////////////////////////////////
+#define pv(v)             \
+    for (auto i : v)      \
+        cout << i << ' '; \
+    cout << endl;
+#define cinv(v, n)              \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+//////////////////////////////////////////////////
+const int maxN = 200005;
+typedef long long ll;
+#define int long long
+#define MOD 1000000007
+#define inf 0x3f3f3f3f
+
+///////////////////////////////////////////////////
+#define mem(a, t) memset(a, t, sizeof(a))
+#define endl '\n'
+template <typename T>
+void print(T &&t)
+{
+    cout << t << '\n';
+}
+template <typename T, typename... Args>
+void print(T &&t, Args &&...args)
+{
+    cout << t << ' ';
+    print(forward<Args>(args)...);
+}
+
+///////////////////////////////////////////////////
+#define pb push_back
+#define Pb pop_back
+#define be() begin()
+#define en() end()
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define len(p) (ll) p.size()
+
+///////////////////////////////////////////////////
+
+ll handle_mod(ll n, ll mod = MOD)
+{
+    return (n < 0 ? mod - abs(n % mod) : n) % mod;
+}
+
+ll binpow(ll a, ll b, ll m = 1e18)
+{
+    a %= m;
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+void pre()
+{
+    return;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    cinv(v, n);
+    int l = 0, r = n - 1;
+    // sort the array and store it in a new array
+    vector<int> v1 = v;
+    sort(all(v1));
+    int high = n - 1, low = 0;
+    while (l < r && low < high)
+    {
+        if (v[l] == v1[low])
+        {
+            l++;
+            low++;
+        }
+        else if (v[l] == v1[high])
+        {
+            l++;
+            high--;
+        }
+        else if (v[r] == v1[low])
+        {
+            r--;
+            low++;
+        }
+        else if (v[r] == v1[high])
+        {
+            r--;
+            high--;
+        }
+        else
+            break;
+    }
+    if (l < r)
+        cout << l + 1 << " " << r + 1 << endl;
+    else
+        cout << -1 << endl;
+}
+
+signed main()
+{
+    fast_io;
+    pre();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
