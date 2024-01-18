d9a0a027eddf2504fb0fb9a548eedecb2f79b595
Starkprince <games.princeraj@gmail.com>
2024-01-18 12:35:19 +0530

    started it all again

---
+++ C_Manhattan_Subarrays.cpp
@@ -0,0 +1,128 @@
+// ?Problem :
+// *Solution :
+
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////////////////////////////////////////
+/*
+██████╗░██████╗░██╗███╗░░██╗░█████╗░███████╗  ██████╗░░█████╗░░░░░░██╗
+██╔══██╗██╔══██╗██║████╗░██║██╔══██╗██╔════╝  ██╔══██╗██╔══██╗░░░░░██║
+██████╔╝██████╔╝██║██╔██╗██║██║░░╚═╝█████╗░░  ██████╔╝███████║░░░░░██║
+██╔═══╝░██╔══██╗██║██║╚████║██║░░██╗██╔══╝░░  ██╔══██╗██╔══██║██╗░░██║
+██║░░░░░██║░░██║██║██║░╚███║╚█████╔╝███████╗  ██║░░██║██║░░██║╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚════╝░╚══════╝  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░
+*/
+///////////////////////////////////////////////////
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);                 \
+    cout << fixed << setprecision(9);
+///////////////////////////////////////////////////
+#define pv(v)             \
+    for (auto i : v)      \
+        cout << i << ' '; \
+    cout << endl;
+#define cinv(v, n)              \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+//////////////////////////////////////////////////
+const int maxN = 200005;
+typedef long long ll;
+#define int long long
+#define MOD 1000000007
+#define inf 0x3f3f3f3f
+
+///////////////////////////////////////////////////
+#define mem(a, t) memset(a, t, sizeof(a))
+#define endl '\n'
+template <typename T>
+void print(T &&t)
+{
+    cout << t << '\n';
+}
+template <typename T, typename... Args>
+void print(T &&t, Args &&...args)
+{
+    cout << t << ' ';
+    print(forward<Args>(args)...);
+}
+
+///////////////////////////////////////////////////
+#define pb push_back
+#define Pb pop_back
+#define be() begin()
+#define en() end()
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define len(p) (ll) p.size()
+
+///////////////////////////////////////////////////
+
+ll handle_mod(ll n, ll mod = MOD)
+{
+    return (n < 0 ? mod - abs(n % mod) : n) % mod;
+}
+
+ll binpow(ll a, ll b, ll m = 1e18)
+{
+    a %= m;
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+void pre()
+{
+    return;
+}
+
+bool ok;
+int t, n, i, ans, j, k, p;
+
+void solve()
+{
+    cin >> n;
+    ans = 2 * n - 1;
+    int a[n];
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+    for (i = 0; i < n - 2; i++)
+    {
+        if ((a[i] >= a[i + 1] && a[i + 1] >= a[i + 2]) || (a[i] <= a[i + 1] && a[i + 1] <= a[i + 2]))
+            continue;
+        ans++;
+    }
+    for (i = 0; i < n - 3; i++)
+    {
+        ok = true;
+        for (j = i; j < i + 4; j++)
+            for (p = j + 1; p < i + 4; p++)
+                for (k = p + 1; k < i + 4; k++)
+                    if ((a[j] >= a[p] && a[p] >= a[k]) || (a[j] <= a[p] && a[p] <= a[k]))
+                        ok = false;
+        if (ok)
+            ans++;
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    fast_io;
+    pre();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
